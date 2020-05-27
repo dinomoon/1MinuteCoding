37c61feb685777bdf0fc7a2a36edb7a00f8c665b
dinomoon <ansrud45@gmail.com>
2020-05-27 10:13:11 +0900

    전진! 3D스크11

--- 인터랙티브 웹개발 제대로 시작하기/11.전진! 3D스크롤/js/wall3D.js
+++ 인터랙티브 웹개발 제대로 시작하기/11.전진! 3D스크롤/js/wall3D.js
@@ -2,6 +2,8 @@
   const houseElem = document.querySelector(".house");
   const progressBar = document.querySelector(".progress-bar");
   let maxScrollValue;
+  const mousePos = { x: 0, y: 0 };
+  const stageElem = document.querySelector(".stage");
 
   const resizeHandler = () => {
     maxScrollValue = document.body.offsetHeight - window.innerHeight;
@@ -16,6 +18,15 @@
   });
 
   window.addEventListener("resize", resizeHandler);
+
+  window.addEventListener("mousemove", (e) => {
+    mousePos.x = -1 + (e.clientX / window.innerWidth) * 2;
+    mousePos.y = -1 + (e.clientY / window.innerHeight) * 2;
+
+    stageElem.style.transform = `rotateX(${-mousePos.y * 6}deg) rotateY(${
+      mousePos.x * 6
+    }deg)`;
+  });
 
   resizeHandler();
 })();
